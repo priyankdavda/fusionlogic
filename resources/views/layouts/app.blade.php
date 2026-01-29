cd642d2f50ce2621c19eb2e7ebee18fac3c5fef6
priyankdavda <davdapriyank3883@gmail.com>
2026-01-29 20:40:49 +0530

    Add Tawk.to live chat integration script to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,5 +19,19 @@
 <script src="{{ asset('js/swiper.min.js') }}"></script>
 <script src="{{ asset('js/main.js') }}"></script>
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/697b76e72893b51c32bbdb9f/1jg54gu60';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
+    
 </body>
 </html>
